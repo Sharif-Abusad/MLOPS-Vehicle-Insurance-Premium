6ef72df36a6ff1eb6e31034ea7d9d8c875735cfd
Sharif Abusad <abusharif866@gmail.com>
2026-07-14 15:40:56 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sharif Abusad",
+    author_email="abusharif866@gmail.com",
+    packages=find_packages()
+)
