7def37b49561ee4a67c427fced5b230f9339b2ea
Sharif Abusad <abusharif866@gmail.com>
2026-07-17 15:25:15 +0530

    model trainer added

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -5,17 +5,20 @@ from src.logger import logging
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 from src.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
-    DataTransformationConfig
+    DataTransformationConfig,
+    ModelTrainerConfig
 )
 
 from src.entity.artifact_entity import (
     DataIngestionArtifact,
     DataValidationArtifact,
-    DataTransformationArtifact
+    DataTransformationArtifact,
+    ModelTrainerArtifact
 )
 
 
@@ -25,6 +28,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
     
     def start_data_ingestion(self) -> DataIngestionArtifact:
@@ -91,6 +95,28 @@ class TrainPipeline:
         except Exception as e:
             raise MyException(e, sys)
 
+
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        logging.info("Entered the start_model_trainer method of TrainPipeline class")
+        try:
+            model_trainer = ModelTrainer(
+                data_transformation_artifact=data_transformation_artifact,
+                model_trainer_config=self.model_trainer_config
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+            logging.info("Performed Model Training")
+            logging.info("Exited the start_model_trainer method of TrainPipeline class")
+
+            return model_trainer_artifact
+        
+        except Exception as e:
+            raise MyException(e, sys)
+
+
     def run_pipeline(self) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
@@ -102,6 +128,7 @@ class TrainPipeline:
                 data_ingestion_artifact=data_ingestion_artifact,
                 data_validation_artifact=data_validation_artifact
             )
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
 
         except Exception as e:
             raise MyException(e, sys)
